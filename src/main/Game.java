345221ba49f594a04d28c8bd6422c1c6ced0a127
Kian Bennett <kian1066@yahoo.co.uk>
2014-02-04 18:39:27 +0000

    Initial commit

---
+++ src/main/Game.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Game {
+	
+	public Game() {
+		System.out.println("THIS IS A GAME");
+	}
+}
---
+++ src/main/Loader.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Loader {
+	
+	public static void main(String args[]) {
+		new Game();
+	}
+}
